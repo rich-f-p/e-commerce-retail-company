8267839e9a704721722f9b3465980a856c04438d
rich p <richardferry3@gmail.com>
2022-05-05 16:20:01 -0700

    add category put and delete route so categories can be updated or deleted

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,10 +42,38 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteId = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!deleteId) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+    res.status(200).json(deleteId);
+  } catch (err) {
+    res.status(500).json(err);
+}
 });
 
 module.exports = router;
