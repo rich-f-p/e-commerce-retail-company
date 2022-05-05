f9e69d851730fa6d629cfff0be0e87603eeb2ab3
rich p <richardferry3@gmail.com>
2022-05-05 15:50:42 -0700

    add get routes for tag so that a response of all tags and tag by id are served upon request

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: [{ model: Product, through:ProductTag}],
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err)
+    console.log(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findByPk(req.params.id,{
+      include: [{ model: Product, through:ProductTag}],
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err)
+    console.log(err)
+  }
 });
 
 router.post('/', (req, res) => {
