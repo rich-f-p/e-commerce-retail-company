79486ada8e43f8c9cb061e1fd55862ee5f593a6e
rich p <richardferry3@gmail.com>
2022-05-05 07:32:32 -0700

    add get and post routes that will grab category data and create a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,41 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const catergories = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(catergories);
+  } catch (err) {
+    res.status(500).json(err)
+    console.log(err)
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const id = await Category.findByPk(req.params.id,{
+      include: [{model: Product}],
+  });
+    res.status(200).json(id)
+  } catch (err){
+    res.status(500).json(err)
+  }
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCat = await Category.create(req.body)
+    res.status(200).json(newCat);
+  }catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
