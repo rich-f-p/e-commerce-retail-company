42948b6c5f9988e99bf5da7dc008a07c169eea74
rich p <richardferry3@gmail.com>
2022-05-05 18:37:34 -0700

    add delete route for product so items can be deleted from the database

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,8 +107,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteId = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!deleteId) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+    res.status(200).json(deleteId);
+  } catch (err) {
+    res.status(500).json(err);
+}
 });
 
 module.exports = router;
